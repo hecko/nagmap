c461d0b60f023c4a51ac3357c05c38cec92cb3c4
Marcel Hecko <maco@blava.net>
2012-12-03 21:32:38 +0000

    version bump

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $page = $_SERVER['PHP_SELF'];
 $sec = "300"; 
 header("Refresh: $sec; url=$page");
-$nagmap_version = '1.0';
+$nagmap_version = '1.1';
 include('./config.php');
 include('./call.php');
 ?>
